d017e7baf9b56a3922a38d59510b43e0c49228ac
JustTheMaster <buchli.nils@outlook.com>
2025-08-22 22:34:05 +0200

    Floating tool bar isch iz am start! Mi cha scho switche.

---
+++ mobile_application/lib/screens/planning_screen/planning_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_application/screens/shared_for_screens/bottom_navigation_bar.dart';
+import 'package:mobile_application/screens/shared_for_screens/screen_enum.dart';
+
+class PlanningScreen extends StatelessWidget {
+  const PlanningScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar : ScreenBottomNavigationBar(currentScreen: AppScreensEnum.planning)
+    ) ; 
+  }
+}
